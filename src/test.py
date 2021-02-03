b3cff6c71bfbe80c9ef8bca4c612f053485239b0
nickkir <nicholaskiriazis@gmail.com>
2021-02-03 18:19:44 -0500

    Added a function to test the rim detection algorithm

--- src/test.py
+++ src/test.py
@@ -4,34 +4,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
+def test_rim_finding(image_path, color):
+    rim = cv2.imread(image_path)
+    rim_rgb = cv2.cvtColor(rim, cv2.COLOR_BGR2RGB)
+    rim_hsv = cv2.cvtColor(rim_rgb, cv2.COLOR_RGB2HSV)
 
-image_path = "C:\\Users\\Nicholas\\Desktop\\basketball_detection_tests\\rims\\rim4.jpg"
-rim = cv2.imread(image_path)
-rim_rgb = cv2.cvtColor(rim, cv2.COLOR_BGR2RGB)
-rim_rgb_2 = cv2.cvtColor(rim, cv2.COLOR_BGR2RGB)
-rim_rgb_3 = cv2.cvtColor(rim, cv2.COLOR_BGR2RGB)
-rim_hsv = cv2.cvtColor(rim_rgb, cv2.COLOR_RGB2HSV)
+    noisy_mask = bip.get_mask(rim_hsv, color)
+    blurred_mask = bip.apply_median_blur(noisy_mask, passes=3)
+    canny_mask = bip.apply_canny(blurred_mask)
 
-current_color = (7, 255, 237)
+    rim_finding_obj = rf.RimFinder(canny_mask)
 
-noisy_mask = bip.get_mask(rim_hsv, current_color)
-blurred_mask = bip.apply_median_blur(noisy_mask, passes=3)
-canny_mask = bip.apply_canny(blurred_mask)
+    ellipse = rim_finding_obj.find_rim()
 
-
-fig = plt.figure()
-
-test = rf.RimFinder(canny_mask)
-
-
-true_ellipse = test.find_rim()
-cv2.ellipse(rim_rgb, true_ellipse, (0,255,0))
-plt.imshow(rim_rgb)
-
-ellipse = test.get_inner_ellipse()
-
-cv2.ellipse(rim_rgb_2, ellipse, (0,255,0))
-
-plt.show()
+    cv2.ellipse(rim_rgb, ellipse, (0, 255, 0))
+    plt.imshow(rim_rgb)
+    plt.show()
 
 

be0b74d94969a108d6ca4bf622be81f09baeb4a9
nickkir <nicholaskiriazis@gmail.com>
2021-02-03 18:43:04 -0500

    Adding some examples of what the rim detection and ball tracking algorithms are doing

--- src/test.py
+++ src/test.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
-def test_rim_finding(image_path, color):
+def test(image_path, color):
     rim = cv2.imread(image_path)
     rim_rgb = cv2.cvtColor(rim, cv2.COLOR_BGR2RGB)
     rim_hsv = cv2.cvtColor(rim_rgb, cv2.COLOR_RGB2HSV)
@@ -20,5 +20,12 @@ def test_rim_finding(image_path, color):
     cv2.ellipse(rim_rgb, ellipse, (0, 255, 0))
     plt.imshow(rim_rgb)
     plt.show()
+
+
+
+image_path = "C:\\Users\\Nicholas\\Desktop\\basketball_detection_tests\\rims\\rim4.jpg"
+current_color = (7, 255, 237)
+
+test(image_path, current_color)
 
 
